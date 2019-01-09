a9b43ce3fb58d35fb32fd69134e222e9a1006e78
cole.nading <cole.nading@mu-c1-16.mullen.pvt.k12.co.us>
2019-01-09 12:24:11 -0700

    Working on the thing

--- Dungeons and Dragons/src/PlayGame.java
+++ Dungeons and Dragons/src/PlayGame.java
@@ -4,22 +4,24 @@ public class PlayGame {
 	static Scanner userInput = new Scanner(System.in);
 	static boolean playAgain = true;
 	static Character hero;
+	
+	
 	public static void main(String[] args) {
-		while (playAgain) {
+			{
 			intro();
-			//missionOne();
-			playAgain = false;
-		}
+			}
 
 	}
-
+	
 	public static void intro() {
+		hero=new Character("", 0, 0, 0, 0, 0, 0, 0, 0, 0, "", 0, 0);
 		boolean spellingError = true;
 		System.out.println("Welcome to Dungeons and Dragons! For the entirety of this game,"
 				+ " you'll need to press enter to see what to do next!");
 		userInput.nextLine();
 		System.out.println("Let's begin with creating you, kind hero, what is your name?");
 		String name = userInput.nextLine();
+		hero.setName(name);
 		System.out.println(name + ", you will begin with choosing your class, this will define"
 				+ " your starting weapons, abilities, strengths, and weaknesses.");
 		userInput.nextLine();
@@ -28,6 +30,7 @@ public class PlayGame {
 					+ " When you've made your decision type the class you want.");
 
 			String job = userInput.nextLine();
+			hero.setJob(job);
 			switch (job) {
 			case "Knight":
 				System.out.println("Noble choice, the knight- Heavily armored, starts with a two-handed longsword,"
@@ -65,7 +68,7 @@ public class PlayGame {
 				spellingError = true;
 				System.out.println("Error...must only capitalize first letter and spell correctly");
 			}
-			System.out.println("You are now, " + name + " the " + job);
+			System.out.println("You are now, " + hero.getName() + " the " + job);
 			userInput.nextLine();
 			System.out.println(
 					"We will begin rolling 3 6 sided dice, this will fairly determine your statistics for the rest"
@@ -76,6 +79,7 @@ public class PlayGame {
 			int strength = 0;
 			strength = CharacterGenerator.rollDice();
 			System.out.println("You rolled a " + strength);
+			hero.setStrength(strength);
 			//
 			userInput.nextLine();
 			System.out.println("This roll will determine your dexterity");
@@ -83,6 +87,7 @@ public class PlayGame {
 			int dexterity = 0;
 			dexterity = CharacterGenerator.rollDice();
 			System.out.println("You rolled a " + dexterity);
+			
 			//
 			userInput.nextLine();
 			System.out.println("This roll will determine your constitution");
@@ -90,6 +95,7 @@ public class PlayGame {
 			int constitution = 0;
 			constitution = CharacterGenerator.rollDice();
 			System.out.println("You rolled a " + constitution);
+			
 			//
 			userInput.nextLine();
 			System.out.println("This roll will determine your intelligence");
@@ -97,6 +103,7 @@ public class PlayGame {
 			int intelligence = 0;
 			intelligence = CharacterGenerator.rollDice();
 			System.out.println("You rolled a " + intelligence);
+			
 			//
 			userInput.nextLine();
 			System.out.println("This roll will determine your wisdom");
@@ -104,6 +111,7 @@ public class PlayGame {
 			int wisdom = 0;
 			wisdom = CharacterGenerator.rollDice();
 			System.out.println("You rolled a " + wisdom);
+			
 			//
 			userInput.nextLine();
 			System.out.println("This roll will determine your charisma");
@@ -111,129 +119,181 @@ public class PlayGame {
 			int charisma = 0;
 			charisma = CharacterGenerator.rollDice();
 			System.out.println("You rolled a " + charisma);
+			
 			int height=0;
 			int weight=0;
 			int level=1;
 			int hitPoints=10;
-			int experiencePoints=1;
+			int ExperiencePoints=1;
 			System.out.println("Because you are indeed " + name + " the " + job + " your final statistics are...");
 			userInput.nextLine();
 
 			switch (job) {
 			case "Knight":
 				height= height + 70;
+				hero.setHeight(height);
 				weight=weight+210;
+				hero.setWeight(weight);
 				hitPoints=hitPoints+4;
+				hero.setHitPoints(hitPoints);
 				strength = strength + 4;
+				hero.setStrength(strength);
 				dexterity = dexterity - 1;
+				hero.setDexterity(dexterity);
 				constitution = constitution + 2;
+				hero.setConstitution(constitution);
 				intelligence = intelligence - 2;
+				hero.setIntelligence(intelligence);
 				wisdom = wisdom - 3;
+				hero.setWisdom(wisdom);
 				charisma = charisma + 3;
+				hero.setCharisma(charisma);
 				// difference of +3
-				System.out.println(name + " the " + job + " stats:" + " Strength: " + strength + " Dexterity: "
-						+ dexterity + " Constitution: " + constitution + " Intelligence: " + intelligence + " Wisdom: "
-						+ wisdom + " Charisma: " + charisma + " Height: " + height + " Weight: " + weight + 
-						" Level: " + level  + " Hitpoints: " + hitPoints);
+				System.out.println(hero.getName() + " the " + hero.getJob() + " stats:" + " Strength: " + hero.getStrength() + " Dexterity: "
+						+ hero.getDexterity() + " Constitution: " + hero.getConstitution() + " Intelligence: " + hero.getIntelligence() + " Wisdom: "
+						+ hero.getWisdom() + " Charisma: " + hero.getCharisma() + " Height: " + hero.getHeight() + " Inches" + " Weight: " + hero.getWeight() + "Pounds "
+						+" Level: " + hero.getLevel()  + " Hitpoints: " + hero.getHitPoints() + " Experience Points: " + hero.getExperiencePoints());
 				System.out.println("Your statistics will give you advantages or disadvantages throughout the game.");
 				break;
 			case "Barbarian":
 				height= height + 78;
+				hero.setHeight(height);
 				weight=weight+245;
+				hero.setWeight(weight);
 				hitPoints=hitPoints+3;
+				hero.setHitPoints(hitPoints);
 				strength = strength + 3;
+				hero.setStrength(strength);
 				dexterity = dexterity + 1;
+				hero.setDexterity(dexterity);
 				constitution = constitution + 1;
+				hero.setConstitution(constitution);
 				intelligence = intelligence - 3;
+				hero.setIntelligence(intelligence);
 				wisdom = wisdom - 2;
+				hero.setWisdom(wisdom);
 				charisma = charisma + 2;
-				// difference of +2
-				System.out.println(name + " the " + job + " stats:" + " Strength: " + strength + " Dexterity: "
-						+ dexterity + " Constitution: " + constitution + " Intelligence: " + intelligence + " Wisdom: "
-						+ wisdom + " Charisma: " + charisma + " Height: " + height + " Weight: " + weight 
-						+ " Level: " + level  + " Hitpoints: " + hitPoints);
+				hero.setCharisma(charisma);
+				// difference of +3
+				System.out.println(hero.getName() + " the " + hero.getJob() + " stats:" + " Strength: " + hero.getStrength() + " Dexterity: "
+						+ hero.getDexterity() + " Constitution: " + hero.getConstitution() + " Intelligence: " + hero.getIntelligence() + " Wisdom: "
+						+ hero.getWisdom() + " Charisma: " + hero.getCharisma() + " Height: " + hero.getHeight() + " Inches" + " Weight: " + hero.getWeight() + "Pounds "
+						+" Level: " + hero.getLevel()  + " Hitpoints: " + hero.getHitPoints() + " Experience Points: " + hero.getExperiencePoints());
 				System.out.println("Your statistics will give you advantages or disadvantages throughout the game.");
 				break;
 			case "Monk":
 				height= height + 68;
-				weight=weight+185;
+				hero.setHeight(height);
+				weight=weight+ 185;
+				hero.setWeight(weight);
 				hitPoints=hitPoints+3;
+				hero.setHitPoints(hitPoints);
 				strength = strength + 4;
+				hero.setStrength(strength);
 				dexterity = dexterity + 3;
+				hero.setDexterity(dexterity);
 				constitution = constitution + 1;
+				hero.setConstitution(constitution);
 				intelligence = intelligence - 2;
+				hero.setIntelligence(intelligence);
 				wisdom = wisdom + 2;
+				hero.setWisdom(wisdom);
 				charisma = charisma - 4;
-				// difference of +4
-				System.out.println(name + " the " + job + " stats:" + " Strength: " + strength + " Dexterity: "
-						+ dexterity + " Constitution: " + constitution + " Intelligence: " + intelligence + " Wisdom: "
-						+ wisdom + " Charisma: " + charisma + " Height: " + height + " Weight: " + weight + 
-						" Level: " + level  + " Hitpoints: " + hitPoints);
+				hero.setCharisma(charisma);
+				// difference of +3
+				System.out.println(hero.getName() + " the " + hero.getJob() + " stats:" + " Strength: " + hero.getStrength() + " Dexterity: "
+						+ hero.getDexterity() + " Constitution: " + hero.getConstitution() + " Intelligence: " + hero.getIntelligence() + " Wisdom: "
+						+ hero.getWisdom() + " Charisma: " + hero.getCharisma() + " Height: " + hero.getHeight() + " Inches" + " Weight: " + hero.getWeight() + "Pounds "
+						+" Level: " + hero.getLevel()  + " Hitpoints: " + hero.getHitPoints() + " Experience Points: " + hero.getExperiencePoints());
 				System.out.println("Your statistics will give you advantages or disadvantages throughout the game.");
 				break;
 			case "Archer":
 				height= height + 65;
+				hero.setHeight(height);
 				weight=weight+160;
+				hero.setWeight(weight);
 				hitPoints=hitPoints+2;
-				strength = strength - 2;
+				hero.setHitPoints(hitPoints);
+				strength = strength -2;
+				hero.setStrength(strength);
 				dexterity = dexterity + 3;
-				constitution = constitution - 1;
+				hero.setDexterity(dexterity);
+				constitution = constitution -1;
+				hero.setConstitution(constitution);
 				intelligence = intelligence + 2;
+				hero.setIntelligence(intelligence);
 				wisdom = wisdom - 2;
+				hero.setWisdom(wisdom);
 				charisma = charisma + 2;
-				// difference of +2
-				System.out.println(name + " the " + job + " stats:" + " Strength: " + strength + " Dexterity: "
-						+ dexterity + " Constitution: " + constitution + " Intelligence: " + intelligence + " Wisdom: "
-						+ wisdom + " Charisma: " + charisma + " Height: " + height + " Weight: " + weight + 
-						" Level: " + level + " Hitpoints: " + hitPoints);
+				hero.setCharisma(charisma);
+				// difference of +3
+				System.out.println(hero.getName() + " the " + hero.getJob() + " stats:" + " Strength: " + hero.getStrength() + " Dexterity: "
+						+ hero.getDexterity() + " Constitution: " + hero.getConstitution() + " Intelligence: " + hero.getIntelligence() + " Wisdom: "
+						+ hero.getWisdom() + " Charisma: " + hero.getCharisma() + " Height: " + hero.getHeight() + " Inches" + " Weight: " + hero.getWeight() + "Pounds "
+						+" Level: " + hero.getLevel()  + " Hitpoints: " + hero.getHitPoints() + " Experience Points: " + hero.getExperiencePoints());
 				System.out.println("Your statistics will give you advantages or disadvantages throughout the game.");
 				break;
 			case "Assasin":
 				height= height + 62;
-				weight=weight+255;
+				hero.setHeight(height);
+				weight=weight+180;
+				hero.setWeight(weight);
 				hitPoints=hitPoints+2;
+				hero.setHitPoints(hitPoints);
 				strength = strength + 2;
+				hero.setStrength(strength);
 				dexterity = dexterity + 4;
-				constitution = constitution - 1;
+				hero.setDexterity(dexterity);
+				constitution = constitution -1;
+				hero.setConstitution(constitution);
 				intelligence = intelligence + 3;
+				hero.setIntelligence(intelligence);
 				wisdom = wisdom - 3;
+				hero.setWisdom(wisdom);
 				charisma = charisma - 2;
+				hero.setCharisma(charisma);
 				// difference of +3
-				System.out.println(name + " the " + job + " stats:" + " Strength: " + strength + " Dexterity: "
-						+ dexterity + " Constitution: " + constitution + " Intelligence: " + intelligence + " Wisdom: "
-						+ wisdom + " Charisma: " + charisma + " Height: " + height + " Weight: " + weight +
-						" Level: " + level + " Hitpoints: " + hitPoints);
+				System.out.println(hero.getName() + " the " + hero.getJob() + " stats:" + " Strength: " + hero.getStrength() + " Dexterity: "
+						+ hero.getDexterity() + " Constitution: " + hero.getConstitution() + " Intelligence: " + hero.getIntelligence() + " Wisdom: "
+						+ hero.getWisdom() + " Charisma: " + hero.getCharisma() + " Height: " + hero.getHeight() + " Inches" + " Weight: " + hero.getWeight() + "Pounds "
+						+" Level: " + hero.getLevel()  + " Hitpoints: " + hero.getHitPoints() + " Experience Points: " + hero.getExperiencePoints());
 				System.out.println("Your statistics will give you advantages or disadvantages throughout the game.");
 				break;
 			case "Wizard":
 				height= height + 85;
+				hero.setHeight(height);
 				weight=weight+220;
+				hero.setWeight(weight);
 				hitPoints=hitPoints+1;
-				strength = strength - 3;
+				hero.setHitPoints(hitPoints);
+				strength = strength -3;
+				hero.setStrength(strength);
 				dexterity = dexterity - 2;
+				hero.setDexterity(dexterity);
 				constitution = constitution + 2;
+				hero.setConstitution(constitution);
 				intelligence = intelligence + 4;
+				hero.setIntelligence(intelligence);
 				wisdom = wisdom + 3;
+				hero.setWisdom(wisdom);
 				charisma = charisma - 1;
+				hero.setCharisma(charisma);
 				// difference of +3
-				System.out.println(name + " the " + job + " stats:" + " Strength: " + strength + " Dexterity: "
-						+ dexterity + " Constitution: " + constitution + " Intelligence: " + intelligence + " Wisdom: "
-						+ wisdom + " Charisma: " + charisma + " Height: " + height + " Weight: " + weight 
-						+ " Level: " + level + " Hitpoints: " + hitPoints);
+				System.out.println(hero.getName() + " the " + hero.getJob() + " stats:" + " Strength: " + hero.getStrength() + " Dexterity: "
+						+ hero.getDexterity() + " Constitution: " + hero.getConstitution() + " Intelligence: " + hero.getIntelligence() + " Wisdom: "
+						+ hero.getWisdom() + " Charisma: " + hero.getCharisma() + " Height: " + hero.getHeight() + " Inches" + " Weight: " + hero.getWeight() + "Pounds "
+						+" Level: " + hero.getLevel()  + " Hitpoints: " + hero.getHitPoints() + " Experience Points: " + hero.getExperiencePoints());
 				System.out.println("Your statistics will give you advantages or disadvantages throughout the game.");
 				break;
-			default:
-				spellingError = true;
-				System.out.println("Error...must only capitalize first letter and spell correctly");
 
 			}
-			hero=new Character(name, level, height, weight, strength, dexterity, constitution, intelligence, wisdom, charisma, job, hitPoints, experiencePoints);
 		}
 	}
 
 	public static void MissionOne()
 	{
-		System.out.println("Hello, " + name + job + "I am King Arthur and I need your help!");
+		String status= hero.getName()+hero.getJob();
+		System.out.println("Hello, " + status + "I am King Arthur and I need your help!");
 		System.out.println("Are you up to the task");
 		String answer=userInput.nextLine();
 		if (answer.equals("Yes")||answer.equals("YES")||answer.equals("yes"));
